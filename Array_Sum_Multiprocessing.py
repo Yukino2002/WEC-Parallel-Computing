8fd53a9fc1a7ded3647d7dc13bfd0f3df67801e2
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2021-11-26 09:54:49 +0530

    Added Comments

--- Array_Sum_Multiprocessing.py
+++ Array_Sum_Multiprocessing.py
@@ -1,35 +1,39 @@
 import multiprocessing
-import time
 
+# __sum is used to calculate the sum of the different array slices
+# we pass all the different parts of the array independently
 def __sum(arr, arr_sum):
     arr_sum.value += sum(arr)
 
 if __name__ == '__main__':
-    arr = [x for x in range(10)]
+    arr = [x for x in range(101)]
     n = len(arr)
 
-    arr_sum = multiprocessing.Value("d", 0.0, lock = False)
-    arr_sum.value = 0
+    # multiprocessing library has .Value("data_type", initial_value, lock) function for sharing data between processes
+    arr_sum = multiprocessing.Value("i", 0, lock = False)
 
+    # initialising a list to store the details of all the processes we opened
     processes = []
-    
-    k = 8
+    # the number of process we want to run independently
+    num_processes = 8
 
     m = n
-    n = (n//k)*k + k
+    # to get the nearest multiple of num_processes, so that we do not miss any elements of the array
+    n = (n//num_processes)*num_processes + num_processes
 
-    st = time.time()
-
-    for i in range(k):
-        p = multiprocessing.Process(target = __sum, args = [arr[i*(n//k): min(m, (i+1)*(n//k))], arr_sum])
+    for i in range(num_processes):
+        # multiprocessing.process(target, args) -> target = function to be executed by the process, args = arguments to be passed in target
+        p = multiprocessing.Process(target = __sum, args = [arr[i*(n//num_processes): min(m, (i+1)*(n//num_processes))], arr_sum])
         processes.append(p)
 
+        # .start() begins the execution of the process
         p.start()
-        print(p.pid)
 
     for p in processes:
+        # while executing other processes, our main function would continue to run, so to stop that from happening
+        # we use .join() which stops the current program till the other processes join
         p.join()
 
-    ed = time.time()
-    print()
-    print(arr_sum.value, ed-st)
+    # displlaying the details of the processes and the sum of the array
+    print(processes, "\n")
+    print(arr_sum.value)
